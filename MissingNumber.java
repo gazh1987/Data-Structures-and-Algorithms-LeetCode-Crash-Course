8ef9869be439c1f0bd3c852af7b72743ecdcbf3e
Gary Healy <gary.healy.87@gmail.com>
2025-03-09 13:09:49 +0000

    Missing Number

---
+++ MissingNumber.java
@@ -0,0 +1,46 @@
+package Dsa;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class MissingNumber {
+	
+	
+	// missinNumber using a set as the exercise is about Hashing.  
+	// Added the much quicker method missingNumberFast which does not use hashing
+	
+	public int missingNumber(int[] nums) {
+		
+		if (nums == null || nums.length < 2) {
+			throw new IllegalArgumentException("Nums Array Length must have more than 1 element");
+		}
+		
+		Set<Integer> numsSet = new HashSet<Integer>();
+		
+		for (int n : nums) {
+			numsSet.add(n);
+		}
+		
+		for (int i = 0; i < nums.length + 1; i ++){
+			if (!numsSet.contains(i)) {
+				return i;
+			}
+		}
+		
+		throw new IllegalStateException("No missing number found");
+    }
+	
+	public int missingNumberFast(int[] nums) {
+		
+		int sumOfNums = 0;
+		int sumOfIndex = 0;
+		for(int i = 0; i < nums.length; i++) {
+			sumOfNums += nums[i];
+			sumOfIndex += i + 1;
+		}
+		
+		return sumOfIndex - sumOfNums;
+	}
+}
---
+++ MissingNumberTest.java
@@ -0,0 +1,67 @@
+package Dsa;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MissingNumberTest {
+
+	MissingNumber m;
+	
+	@BeforeEach
+	void setup() {
+		m = new MissingNumber();
+	}
+	
+	@Test
+	void givenNumsIsNull_thenThrowIllegalArgumentException() {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+			m.missingNumber(null);
+		});
+		
+		assertEquals("Nums Array Length must have more than 1 element", exception.getMessage());
+	}
+	
+	@Test
+	void givenNumsLengthLt2_thenThrowIllegalArgumentException() {
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+			m.missingNumber(new int[] {1});
+		});
+		
+		assertEquals("Nums Array Length must have more than 1 element", exception.getMessage());
+	}
+	
+	@Test
+	void givenNumsLengthIs3_thenReturn2() {
+		assertEquals(2, m.missingNumber(new int[] {3, 0, 1}));
+	}
+	
+	@Test
+	void givenNumsLengthIs2_thenReturn2() {
+		assertEquals(2, m.missingNumber(new int[] {0, 1}));
+	}
+	
+	@Test
+	void givenNumsLengthIs9_thenReturn8() {
+		assertEquals(8, m.missingNumber(new int[] {9,6,4,2,3,5,7,0,1}));
+	}
+	
+	@Test
+	void givenNumsLengthIs3_whenMissingNumsFast_thenReturn2() {
+		assertEquals(2, m.missingNumberFast(new int[] {3, 0, 1}));
+	}
+	
+	@Test
+	void givenNumsLengthIs2_whenMissingNumsFast_thenReturn2() {
+		assertEquals(2, m.missingNumberFast(new int[] {0, 1}));
+	}
+	
+	@Test
+	void givenNumsLengthIs9_whenMissingNumsFast_thenReturn8() {
+		assertEquals(8, m.missingNumberFast(new int[] {9,6,4,2,3,5,7,0,1}));
+	}
+	
+}
