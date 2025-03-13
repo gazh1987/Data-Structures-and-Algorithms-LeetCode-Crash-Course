409fe4dd57a518f676f28d165c79ec365d0c4d1b
Gary Healy <gary.healy.87@gmail.com>
2025-03-13 22:45:43 +0000

    Intersection of multiple arrays

---
+++ IntersectionOfMultipleArrays.java
@@ -0,0 +1,34 @@
+package Dsa;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IntersectionOfMultipleArrays {
+	
+	public List<Integer> intersection(int[][] nums) {
+		
+		if (nums == null || nums.length == 0) {
+			return Collections.emptyList();
+		}
+		
+        List<Integer> intersectedValues = new ArrayList<>();
+        Map<Integer, Integer> elementFrequencyMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+            	elementFrequencyMap.put(nums[i][j], elementFrequencyMap.getOrDefault(nums[i][j], 0) + 1);
+            	
+            	if (elementFrequencyMap.get(nums[i][j]) == nums.length) {
+            		intersectedValues.add(nums[i][j]);
+            	}
+            }
+        }
+        
+        Collections.sort(intersectedValues);
+        
+        return intersectedValues;   
+	}
+}
---
+++ IntersectionOfMultipleArraysTest.java
@@ -0,0 +1,60 @@
+package Dsa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IntersectionOfMultipleArraysTest {
+
+	IntersectionOfMultipleArrays i;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		i = new IntersectionOfMultipleArrays();
+	}
+
+	@Test
+	void givenNumbersExistInAllArrays_returnIntersectedNumbers_Test1() {
+		int[][] nums = {{3,1,2,4,5},{1,2,3,4},{3,4,5,6}};
+		List<Integer> expected = new ArrayList<>();
+		expected.add(3);
+		expected.add(4);
+		assertEquals(expected, i.intersection(nums));
+	}
+	
+	@Test
+	void givenNumbersExistInAllArrays_returnIntersectedNumbers_Test2() {
+		int[][] nums = {{7,34,45,10,12,27,13},{27,21,45,10,12,13}};
+		List<Integer> expected = new ArrayList<>();
+		expected.add(10);
+		expected.add(12);
+		expected.add(13);
+		expected.add(27);
+		expected.add(45);
+		assertEquals(expected, i.intersection(nums));
+	}
+	
+	@Test
+	void givenNumbersDoNotExistInAllArrays_returnEmptyList() {
+		int[][] nums = {{1, 2, 3}, {4, 5, 6}};
+		List<Integer> expected = new ArrayList<>();
+		assertEquals(expected, i.intersection(nums));
+	}
+	
+	@Test
+	void givenNumsIsNull_returnEmptyList() {
+		List<Integer> expected = new ArrayList<>();
+		assertEquals(expected, i.intersection(null));
+	}
+	
+	@Test
+	void givenNumsIsEmpty_returnEmptyList() {
+		List<Integer> expected = new ArrayList<>();
+		assertEquals(expected, i.intersection(new int[0][0]));
+	}
+
+}
